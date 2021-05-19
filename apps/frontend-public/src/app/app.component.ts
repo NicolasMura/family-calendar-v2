454ecf1cab77e402fdbf19e5a20e5ed44e667f8f
NicolasMura <contact@nicolasmura.fr>
2021-05-19 09:46:42 +0200

    initial commit

---
+++ apps/frontend-public/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'family-calendar-v2-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend-public';
+}
